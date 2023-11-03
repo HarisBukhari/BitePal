1a111d8cb7a95e17ece210efc053f1b3df9cd0c6
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2023-11-03 21:19:46 +0500

    Add on

--- controllers/CustomerController.ts
+++ controllers/CustomerController.ts
@@ -3,29 +3,33 @@ import mongoose from "mongoose"
 import { CreateVendorInput } from "../dto"
 import { Vendor } from "../models"
 import { generateSalt, hashPassword } from "../utilities"
+import { plainToClass } from "class-transformer"
+import { validate } from "class-validator"
+import { CreateCustomerInputs } from "../dto/Customer.dto"
 
 
 export const CustomerSignUp = async (req: Request, res: Response, next: NextFunction) => {
-    
+    const customerInputs = plainToClass(CreateCustomerInputs, req.body)
+    const inputErrors = await validate(customerInputs, {validationError: {target:true}})
 }
 
 export const CustomerLogin = async (req: Request, res: Response, next: NextFunction) => {
-    
+
 }
 
 export const CustomerVerify = async (req: Request, res: Response, next: NextFunction) => {
-    
+
 }
 
 export const OTP = async (req: Request, res: Response, next: NextFunction) => {
-    
+
 }
 
 export const CustomerProfile = async (req: Request, res: Response, next: NextFunction) => {
-    
+
 }
 
 export const UpdateCutomerProfile = async (req: Request, res: Response, next: NextFunction) => {
-    
+
 }
 
---
+++ dto/Customer.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, Length } from "class-validator";
+
+export class CreateCustomerInputs {
+
+    @IsEmail()
+    email: string;
+
+    @Length(7, 12)
+    phone: string;
+
+    @Length(7, 12)
+    password: string;
+
+}
