8811f832c612941985e2cfe47463473190687eb1
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2023-11-28 19:32:51 +0500

    add on

---
+++ dist/config/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppSecret = exports.mongoDB_URI = void 0;
+exports.mongoDB_URI = "mongodb+srv://syedharishussain86:Pr21BBhVUzYqRfv8@cluster0.rxl54ai.mongodb.net/?retryWrites=true&w=majority";
+exports.AppSecret = 'Monkey';
+//# sourceMappingURL=index.js.map
