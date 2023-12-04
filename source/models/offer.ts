f449b55cf76cd0c3ee521d7f795639c94c3f5227
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2023-12-04 11:18:09 +0500

    Update offer.ts

--- source/models/offer.ts
+++ source/models/offer.ts
@@ -2,45 +2,43 @@ import mongoose, { Schema, Document, Model } from 'mongoose';
 
 
 export interface OfferDoc extends Document {
-   offerType: string;
-   vendors: [any];
-   title: string;
-   description: string;
-   minValue: number;
-   offerAmount: number;
-   startValidity: Date;
-   endValidity: Date;
-   promocode: string;
-   promoType: string;
-   bank: [any];
-   bins: [any];
-   pincode: string;
-   isActive: boolean;
+    offerType: string;
+    vendors: [any];
+    title: string;
+    description: string;
+    minValue: number;
+    offerAmount: number;
+    startValidity: Date;
+    endValidity: Date;
+    promocode: string;
+    promoType: string;
+    bank: [any];
+    bins: [any];
+    pincode: string;
+    isActive: boolean;
 }
 
 
 const OfferSchema = new Schema({
-
-    offerType: {type: String, require: true},
+    offerType: { type: String, require: true },
     vendors: [
-        {type: Schema.Types.ObjectId, ref: 'vendor'},
+        { type: Schema.Types.ObjectId, ref: 'vendor' },
     ],
-    title: {type: String, require: true},
-    description: {type: String},
-    minValue: {type: Number, require: true},
-    offerAmount: {type: Number, require: true},
+    title: { type: String, require: true },
+    description: { type: String },
+    minValue: { type: Number, require: true },
+    offerAmount: { type: Number, require: true },
     startValidity: Date,
     endValidity: Date,
-    promocode: {type: String, require: true},
-    promoType: {type: String, require: true},
-    bank: [{ type: String}],
-    bins: [{ type: Number}],
-    pincode: {type: String, require: true},
+    promocode: { type: String, require: true },
+    promoType: { type: String, require: true },
+    bank: [{ type: String }],
+    bins: [{ type: Number }],
+    pincode: { type: String, require: true },
     isActive: Boolean
-  
-},{
+}, {
     toJSON: {
-        transform(doc, ret){
+        transform(doc, ret) {
             delete ret.__v;
             delete ret.createdAt;
             delete ret.updatedAt;
@@ -49,7 +47,6 @@ const OfferSchema = new Schema({
     },
     timestamps: true
 });
-
 
 const Offer = mongoose.model<OfferDoc>('offer', OfferSchema);
 
